a807c9c6990d630ac6c2c68da7a916f84f99bcd7
vexfairyprincess <samora@uamv.edu.ni>
2024-09-10 18:44:32 -0600

    Agregue un arreglo

--- Sesion7/src/main/java/org/example/Main.java
+++ Sesion7/src/main/java/org/example/Main.java
@@ -4,14 +4,15 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+            int[] ages = new int[10];
+            ages[0] = 15;
+            ages[1] = 25;
+            ages[2] = 30;
+            ages[3] = 34;
+            ages[4] = 33;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            for (int i  = 0; i < 5; i++){
+                System.out.println(ages[i]);
         }
     }
 }
