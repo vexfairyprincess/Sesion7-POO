d26dc40215789ab8729d4aa61429a830446121db
andress-ms <amartinezs@uamv.edu.ni>
2024-09-10 18:44:26 -0600

    Agregue un arreglo

---
+++ Sesion7/src/main/java/online/Main.java
@@ -0,0 +1,16 @@
+package online;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] ages = new int[10];
+        ages[0] = 15;
+        ages[1] = 25;
+        ages[2] = 30;
+        ages[3] = 34;
+        ages[4] = 33;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(ages[i]);
+        }
+    }
+}
